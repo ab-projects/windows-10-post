dc5f296e5870abf6b7e7c6f4776d0e2e518cb044
Nils Ballmann <nballmann@ab-projects.de>
2018-12-10 17:46:43 +0100

    even some onedrive removal

--- setup_03_appx.ps1
+++ setup_03_appx.ps1
@@ -210,3 +210,8 @@ Get-AppxPackage *MarchofEmpires | Remove-AppxPackage
 # See How to Completely Uninstall OneDrive in Windows 10 http://lifehacker.com/how-to-completely-uninstall-onedrive-in-windows-10-1725363532
 taskkill /f /im OneDrive.exe
 C:\Windows\SysWOW64\OneDriveSetup.exe /uninstall
+
+#Then type in either
+#%SystemRoot%\System32\OneDriveSetup.exe /uninstall
+#if you’re using 32-bit Windows 10 or
+#%SystemRoot%\SysWOW64\OneDriveSetup.exe /uninstall

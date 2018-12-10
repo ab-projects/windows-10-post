5ab7a0146140c06ff903b542f37db8b08dc3ca9f
Nils Ballmann <nballmann@ab-projects.de>
2018-12-10 17:34:19 +0100

    split up into system steps and user steps

---
+++ setup_04_system.ps1
@@ -0,0 +1,61 @@
+#Requires -Version 3.0
+# -*- coding: utf-8 -*-
+
+param(
+    # Name the new PC
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Computername
+)
+
+$ErrorActionPreference = "Stop"
+
+[string] $myName = $MyInvocation.MyCommand.Name
+[string] $myParentDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+[string] $myExecDir = Get-Location
+
+# Any throw comes through here
+trap {
+    $oldForegroundColor = $host.ui.RawUI.ForegroundColor
+    $host.ui.RawUI.ForegroundColor = "Red"
+    $line = $_.InvocationInfo.ScriptLineNumber
+    Write-Output "${myName}: ERROR: ${line}: $_"
+    $host.ui.RawUI.ForegroundColor = $oldForegroundColor
+    exit 1
+}
+
+#--- Windows Subsystems/Features ---
+#choco install Microsoft-Hyper-V-All -source windowsFeatures
+#choco install Microsoft-Windows-Subsystem-Linux -source windowsfeatures
+
+Disable-WindowsOptionalFeature -Online -FeatureName Internet-Explorer-Optional-amd64
+
+# Uninstall McAfee Security App
+$mcafee = gci "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" | foreach { gp $_.PSPath } | ? { $_ -match "McAfee Security" } | select UninstallString
+if ($mcafee) {
+    $mcafee = $mcafee.UninstallString -Replace "C:\Program Files\McAfee\MSC\mcuihost.exe",""
+    Write "Uninstalling McAfee..."
+    start-process "C:\Program Files\McAfee\MSC\mcuihost.exe" -arg "$mcafee" -Wait
+}
+
+# WiFi Sense: HotSpot Sharing: Disable
+If (-Not (Test-Path "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting")) {
+    New-Item -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting | Out-Null
+}
+Set-ItemProperty -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting -Name value -Type DWord -Value 0
+
+# WiFi Sense: Shared HotSpot Auto-Connect: Disable
+Set-ItemProperty -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots -Name value -Type DWord -Value 0
+
+# Disable Telemetry (requires a reboot to take effect)
+Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection -Name AllowTelemetry -Type DWord -Value 0
+Get-Service DiagTrack,Dmwappushservice | Stop-Service | Set-Service -StartupType Disabled
+
+# Lock screen (not sleep) on lid close
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power' -Name AwayModeEnabled -Type DWord -Value 1
+
+#--- Rename the Computer ---
+# Requires restart, or add the -Restart flag
+if ($env:computername -ne $Computername) {
+    Rename-Computer -NewName $Computername
+}
---
+++ setup_05_user.ps1
@@ -0,0 +1,53 @@
+#Requires -Version 3.0
+# -*- coding: utf-8 -*-
+
+param(
+)
+
+$ErrorActionPreference = "Stop"
+
+[string] $myName = $MyInvocation.MyCommand.Name
+[string] $myParentDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+[string] $myExecDir = Get-Location
+
+# Any throw comes through here
+trap {
+    $oldForegroundColor = $host.ui.RawUI.ForegroundColor
+    $host.ui.RawUI.ForegroundColor = "Red"
+    $line = $_.InvocationInfo.ScriptLineNumber
+    Write-Output "${myName}: ERROR: ${line}: $_"
+    $host.ui.RawUI.ForegroundColor = $oldForegroundColor
+    exit 1
+}
+
+# Privacy: Let apps use my advertising ID: Disable
+If (-Not (Test-Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo")) {
+    New-Item -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo | Out-Null
+}
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo -Name Enabled -Type DWord -Value 0
+
+# Start Menu: Disable Bing Search Results
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 0
+
+# Better File Explorer
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+
+# These make "Quick Access" behave much closer to the old "Favorites"
+# Disable Quick Access: Recent Files
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowRecent -Type DWord -Value 0
+# Disable Quick Access: Frequent Folders
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowFrequent -Type DWord -Value 0
+
+# Disable Xbox Gamebar
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR" -Name AppCaptureEnabled -Type DWord -Value 0
+Set-ItemProperty -Path "HKCU:\System\GameConfigStore" -Name GameDVR_Enabled -Type DWord -Value 0
+
+# Turn off People in Taskbar
+If (-Not (Test-Path "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People")) {
+    New-Item -Path HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People | Out-Null
+}
+Set-ItemProperty -Path "HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" -Name PeopleBand -Type DWord -Value 0
+
+Remove-Item $env:USERPROFILE"\3D Objects"

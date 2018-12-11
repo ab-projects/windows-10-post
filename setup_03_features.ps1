b5b223b46ed2615d9a24d34c8fe77bb01d6488d1
Nils Ballmann <nballmann@ab-projects.de>
2018-12-11 19:11:24 +0100

    features will be uninstalled before appx packages get removed

---
+++ setup_03_features.ps1
@@ -0,0 +1,24 @@
+#Requires -Version 3.0
+# -*- coding: utf-8 -*-
+
+param(
+)
+
+$ErrorActionPreference = "Stop"
+
+[string] $myName = $MyInvocation.MyCommand.Name
+[string] $myParentDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+[string] $myExecDir = Get-Location
+
+# Any throw comes through here
+trap {
+    $oldForegroundColor = $host.ui.RawUI.ForegroundColor
+    $host.ui.RawUI.ForegroundColor = "Red"
+    $line = $_.InvocationInfo.ScriptLineNumber
+    Write-Output "${myName}: ERROR: ${line}: $_"
+    $host.ui.RawUI.ForegroundColor = $oldForegroundColor
+    exit 1
+}
+
+Disable-WindowsOptionalFeature -FeatureName "WindowsMediaPlayer" -Online -NoRestart
+Disable-WindowsOptionalFeature -FeatureName "SearchEngine-Client-Package" -Online -NoRestart

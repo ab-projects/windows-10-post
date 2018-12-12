55096e774a602e6e58b041158a04c13211cfb135
Nils Ballmann <nballmann@ab-projects.de>
2018-12-12 18:01:29 +0100

    some cleaning system stuff

--- setup_06_system.ps1
+++ setup_06_system.ps1
@@ -25,20 +25,6 @@ trap {
     exit 1
 }
 
-#--- Windows Subsystems/Features ---
-#choco install Microsoft-Hyper-V-All -source windowsFeatures
-#choco install Microsoft-Windows-Subsystem-Linux -source windowsfeatures
-
-Disable-WindowsOptionalFeature -Online -FeatureName Internet-Explorer-Optional-amd64
-
-# Uninstall McAfee Security App
-$mcafee = gci "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" | foreach { gp $_.PSPath } | ? { $_ -match "McAfee Security" } | select UninstallString
-if ($mcafee) {
-    $mcafee = $mcafee.UninstallString -Replace "C:\Program Files\McAfee\MSC\mcuihost.exe",""
-    Write "Uninstalling McAfee..."
-    start-process "C:\Program Files\McAfee\MSC\mcuihost.exe" -arg "$mcafee" -Wait
-}
-
 # WiFi Sense: HotSpot Sharing: Disable
 If (-Not (Test-Path "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting")) {
     New-Item -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting | Out-Null
@@ -51,9 +37,6 @@ Set-ItemProperty -Path HKLM:\Software\Microsoft\PolicyManager\default\WiFi\Allow
 # Disable Telemetry (requires a reboot to take effect)
 Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection -Name AllowTelemetry -Type DWord -Value 0
 Get-Service DiagTrack,Dmwappushservice | Stop-Service | Set-Service -StartupType Disabled
-
-# Lock screen (not sleep) on lid close
-#Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power' -Name AwayModeEnabled -Type DWord -Value 1
 
 #--- Rename the Computer ---
 # Requires restart, or add the -Restart flag

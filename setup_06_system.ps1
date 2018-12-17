da08703ea51bb070c7d850c2bdfe5b37b31ca9b7
Nils Ballmann <nballmann@ab-projects.de>
2018-12-17 14:43:06 +0100

    some more progress

--- setup_06_system.ps1
+++ setup_06_system.ps1
@@ -27,19 +27,89 @@ trap {
 
 # TODO: Test this
 
+# Completely uninstall OneDrive
+taskkill /f /im OneDrive.exe
+$one_drive_setup = Join-Path $env:SystemRoot "SysWOW64\OneDriveSetup.exe"
+if ( Test-Path $one_drive_setup ) {
+    & $one_drive_setup /uninstall
+}
+
+$consumer_features = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent'
+if ( -not ( Test-Path $consumer_features ) ) {
+    New-Item -Path $consumer_features | Out-Null
+}
+New-ItemProperty -Path $consumer_features -Name DisableWindowsConsumerFeatures -PropertyType DWORD -Value '1' -Force
+
+# Disabling Microsoft Edge desktop icon creation
+$exp = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer'
+New-ItemProperty -Path $exp -Name 'Explorer' -PropertyType DWORD -Value '1' -Force
+New-ItemProperty -Path $exp -Name 'DisableEdgeDesktopShortcutCreation' -PropertyType DWORD -Value '1' -Force
+
+# Disabling New Network Dialog
+New-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Network' -Name 'NewNetworkWindowOff' -Force
+
 # WiFi Sense: HotSpot Sharing: Disable
 $wifi_sense = "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting"
 if ( -not ( Test-Path $wifi_sense ) ) {
     New-Item -Path $wifi_sense | Out-Null
 }
-Set-ItemProperty -Path $wifi_sense -Name value -Type DWord -Value 0
+Set-ItemProperty -Path $wifi_sense -Name value -Type DWORD -Value 0
 
 # WiFi Sense: Shared HotSpot Auto-Connect: Disable
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots" -Name value -Type DWord -Value 0
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots" -Name value -Type DWORD -Value 0
 
-# Disable Telemetry (requires a reboot to take effect)
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name AllowTelemetry -Type DWord -Value 0
-Get-Service DiagTrack,Dmwappushservice | Stop-Service | Set-Service -StartupType Disabled
+# Disable all feedback notifications
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection' -Name DoNotShowFeedbackNotifications -PropertyType DWORD -Value '1' -Force
+
+# Disable telemetry (requires a reboot to take effect)
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name AllowTelemetry -Type DWORD -Value 0
+Get-Service DiagTrack, Dmwappushservice | Stop-Service | Set-Service -StartupType Disabled
+
+# Disable some services
+# Microsoft Account Sign-in Assistant
+Get-Service wlidsvc | Stop-Service | Set-Service -StartupType Disabled
+# Windows Error Reporting Service
+Get-Service WerSvc | Stop-Service | Set-Service -StartupType Disabled
+# Xbox Live Auth Manager
+Get-Service XblAuthManager | Stop-Service | Set-Service -StartupType Disabled
+# Xbox Live Game Save
+Get-Service XblGameSave | Stop-Service | Set-Service -StartupType Disabled
+# Xbox Live Networking Service
+Get-Service XboxNetApiSvc | Stop-Service | Set-Service -StartupType Disabled
+# Xbox Accessory Management
+Get-Service XboxGipSvc | Stop-Service | Set-Service -StartupType Disabled
+
+# Unregister some scheduled tasks
+Get-ScheduledTask -TaskPath '\Microsoft\XblGameSave\' -TaskName 'XblGameSaveTask' | Unregister-ScheduledTask -Confirm:$false
+
+# Delete some scheduled tasks folders
+$ssO = New-Object -ComObject schedule.service
+$ssO.connect()
+try {
+    $ssO.GetFolder("\Microsoft").DeleteFolder("XblGameSave", $null)
+} catch [System.IO.FileNotFoundException] {
+    # do nothing
+}
+
+# Disable some scheduled tasks
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Application Experience\ProgramDataUpdater"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Application Experience\StartupAppTask"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Autochk\Proxy"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\CloudExperienceHost\CreateObjectTask"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Customer Experience Improvement Program\UsbCeip"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticResolver"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Feedback\Siuf\DmClient"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Feedback\Siuf\DmClientOnScenarioDownload"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Maps\MapsToastTask"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Maps\MapsUpdateTask"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Shell\FamilySafetyMonitor"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Shell\FamilySafetyRefreshTask"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\WDI\ResolutionHost"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Windows Error Reporting\QueueReporting"
+Disable-ScheduledTask -TaskName "\Microsoft\Windows\Windows Media Sharing\UpdateLibrary"
 
 #--- Rename the Computer ---
 # Requires restart, or add the -Restart flag
--- setup_07_user.ps1
+++ setup_07_user.ps1
@@ -26,17 +26,17 @@ $adv_info = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo"
 if ( -not ( Test-Path $adv_info ) ) {
     New-Item -Path $adv_info | Out-Null
 }
-Set-ItemProperty -Path $adv_info -Name Enabled -Type DWord -Value 0
+Set-ItemProperty -Path $adv_info -Name Enabled -Type DWORD -Value 0
 
 # Start Menu: Disable Bing Search Results
-Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWord -Value 0
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name BingSearchEnabled -Type DWORD -Value 0
 
 # These make "Quick Access" behave much closer to the old "Favorites"
 $exp = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer"
 # Disable Quick Access: Recent Files
-Set-ItemProperty -Path $exp -Name ShowRecent -Type DWord -Value 0
+Set-ItemProperty -Path $exp -Name ShowRecent -Type DWORD -Value 0
 # Disable Quick Access: Frequent Folders
-Set-ItemProperty -Path $exp -Name ShowFrequent -Type DWord -Value 0
+Set-ItemProperty -Path $exp -Name ShowFrequent -Type DWORD -Value 0
 
 # Better File Explorer
 $exp_adv = Join-Path $exp "Advanced"
@@ -48,14 +48,14 @@ Set-ItemProperty -Path $exp_adv -Name NavPaneShowAllFolders -Value 1
 Set-ItemProperty -Path $exp_adv -Name MMTaskbarMode -Value 2
 
 # Disable Xbox Gamebar
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR" -Name AppCaptureEnabled -Type DWord -Value 0
-Set-ItemProperty -Path "HKCU:\System\GameConfigStore" -Name GameDVR_Enabled -Type DWord -Value 0
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR" -Name AppCaptureEnabled -Type DWORD -Value 0
+Set-ItemProperty -Path "HKCU:\System\GameConfigStore" -Name GameDVR_Enabled -Type DWORD -Value 0
 
 # Turn off People in Taskbar
 $people = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People"
 if ( -not ( Test-Path $people ) ) {
     New-Item -Path $people | Out-Null
 }
-Set-ItemProperty -Path $people -Name PeopleBand -Type DWord -Value 0
+Set-ItemProperty -Path $people -Name PeopleBand -Type DWORD -Value 0
 
 Remove-Item -Recurse -Force $env:USERPROFILE"\3D Objects"

8f3a7f95afa5e91f9d66b6b731ccf5013fc31f51
Nils Ballmann <nballmann@ab-projects.de>
2018-12-17 18:45:29 +0100

    some ideas for profile fixes

--- setup_08_profiles.ps1
+++ setup_08_profiles.ps1
@@ -30,3 +30,22 @@ Remove-Item -Recurse -Force (Join-Path $default_userprofile '3D Objects')
 
 Remove-Item -Force (Join-Path $env:USERPROFILE 'Desktop\Microsoft Edge.lnk')
 Remove-Item -Force (Join-Path $default_userprofile 'Desktop\Microsoft Edge.lnk')
+
+
+#C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\DefaultLayouts.xml
+
+# <start:SecondaryTile
+#     AppUserModelID="Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy!App"
+#     TileID="PreInstalled.DefaultStartLayout2.1"
+#     Arguments="creative-ms:default?p=DefaultStartLayout2&launch=ms-get-started://redirect%3Fid=placeholdertiles"
+#     IsSuggestedApp="true"
+#     <!-- … -->
+# />
+
+
+
+# TODO: remove the dynamic tiles from startmenue
+#New-Item "C:\temp" -Type Directory
+#Export-StartLayout -Path "C:\temp\DefaultLayout.xml"
+# Delete the files manually please.
+#Import-StartLayout -LayoutPath "C:\temp\DefaultLayout.xml" -MountPath "C:\"

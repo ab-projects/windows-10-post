d1e17bda7c5b2c670235a6b7315ffac436f3e627
Nils Ballmann <nballmann@ab-projects.de>
2018-12-17 17:44:58 +0100

    added profile file level fixes

---
+++ setup_08_profiles.ps1
@@ -0,0 +1,25 @@
+#Requires -Version 3.0
+# -*- coding: utf-8 -*-
+
+param(
+)
+
+$ErrorActionPreference = "Stop"
+
+[string] $myName = $MyInvocation.MyCommand.Name
+[string] $myParentDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+[string] $myExecDir = Get-Location
+
+# Any throw comes through here
+trap {
+    Set-Location $myExecDir | Out-Null
+    $oldForegroundColor = $host.ui.RawUI.ForegroundColor
+    $host.ui.RawUI.ForegroundColor = "Red"
+    $line = $_.InvocationInfo.ScriptLineNumber
+    Write-Output "${myName}: ERROR: ${line}: $_"
+    $host.ui.RawUI.ForegroundColor = $oldForegroundColor
+    exit 1
+}
+
+# TODO: do this for default user profile too 
+Remove-Item -Recurse -Force $env:USERPROFILE"\3D Objects"
